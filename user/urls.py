50cefb0dcd7c5bc808ba552d7d2eaecb840087c1
pg-endokenta <k.endo@4nonome.com>
2022-05-26 22:22:48 +0900

    サインアップ用htmlファイルの名前変更

--- user/urls.py
+++ user/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'user'
 
 urlpatterns = [
-    path('home/', views.HomeView.as_view(), name = 'home')
+    path('signup/', views.SignUpView.as_view(), name = 'signup')
 ]
--- user/views.py
+++ user/views.py
@@ -2,7 +2,5 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 
-
-# Create your views here.
-class HomeView(TemplateView):
-    template_name = "user/index.html"
+class SignUpView(TemplateView):
+    template_name = "user/signup.html"
