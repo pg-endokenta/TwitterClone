28193d0b95cbf9543bcc7b3a6f9acdc7fffe8320
pg-endokenta <k.endo@4nonome.com>
2022-05-27 15:35:28 +0900

    ログイン用ページ追加、メモ追加

--- user/urls.py
+++ user/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'user'
 
 urlpatterns = [
-    path('signup/', views.SignUpView.as_view(), name = 'signup')
+    path('signup/', views.SignUpView.as_view(), name = 'signup'),
+    path("login/", views.LoginView.as_view(), name = 'login'),
 ]
--- user/views.py
+++ user/views.py
@@ -4,3 +4,6 @@ from django.views.generic import TemplateView
 
 class SignUpView(TemplateView):
     template_name = "user/signup.html"
+
+class LoginView(TemplateView):
+    template_name = "user/login.html"
