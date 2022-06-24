9498433e346c286259d52db2b8298aaae532b481
pg-endokenta <k.endo@4nonome.com>
2022-06-24 19:35:27 +0900

    いいね機能の実装

--- tweet/views.py
+++ tweet/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import ListView, CreateView, DetailView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import TC_tweet
+from .models import TC_tweet, Good
+from django.shortcuts import redirect
+from django.contrib import messages
 
 # Create your views here.
 
@@ -33,3 +35,23 @@ class TC_tweetCreateView(LoginRequiredMixin, CreateView):
         #投稿者が誰かを教える。
         form.instance.author_id = self.request.user.id
         return super().form_valid(form)
+
+def good(request, good_id):
+    good_tweet = TC_tweet.objects.get(id=good_id)
+    is_good = Good.objects.filter(owner=request.user) \
+        .filter(tweet=good_tweet).count()
+    if is_good > 0:
+        good_tweet.likes -= 1
+        good_tweet.save()
+        good = Good()
+        Good.objects.filter(owner=request.user).filter(tweet=good_tweet).delete()
+        messages.success(request, 'いいねを取り消しました')
+        return redirect(to='/tweet')
+    good_tweet.likes += 1
+    good_tweet.save()
+    good = Good()
+    good.owner = request.user
+    good.tweet = good_tweet
+    good.save()
+    messages.success(request, 'いいねしました')
+    return redirect(to='/tweet')
